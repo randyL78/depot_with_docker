5a205c1e0b23df74cd91867b81b9253b380aa59b
Randy Layne <randydavidl78@gmail.com>
2019-11-28 18:04:29 -0500

    Create cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_26_134119) do
+ActiveRecord::Schema.define(version: 2019_11_28_225946) do
+
+  create_table "carts", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "title"
