a88230a8e5847ff17bfd93023762d4c8dc726cb8
Randy Layne <randydavidl78@gmail.com>
2019-11-29 00:35:59 -0500

    Add AJAX to sidebar

---
+++ app/views/line_items/create.js.js
@@ -0,0 +1,12 @@
+(function() {
+  var cart;
+
+  cart = document.getElementById("cart");
+
+  cart.innerHTML = "<%= j render(@cart) %>";
+
+}).call(this);
+
+
+//# sourceMappingURL=create.js.js.map
+//# sourceURL=coffeescript
